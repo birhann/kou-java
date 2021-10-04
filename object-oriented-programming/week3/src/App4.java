f8ea03d1bd3bc8bf5c33c36b2550086ea4461906
birhann <birhankarahasan@outlook.com>
2021-10-04 19:01:03 +0300

    beginning

---
+++ object-oriented-programming/week3/src/App4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App4 {
+    public static void main(String[] args) throws Exception {
+        int x = 2;
+        int y = 5;
+        int toplam = x + y;
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
+        System.out.println("Hello" + toplam);
+    }
+}
